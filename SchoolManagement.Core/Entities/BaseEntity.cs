466ef341c52d8c63d33b0542771cd297069bad58
Amir Hamza <hamza_amir@yahoo.com>
2025-09-13 14:42:20 +0600

    Write class BaseEntity

--- SchoolManagement.Core/Entities/BaseEntity.cs
+++ SchoolManagement.Core/Entities/BaseEntity.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Entities
 {
-    internal class BaseEntity
+    public abstract class BaseEntity
     {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
