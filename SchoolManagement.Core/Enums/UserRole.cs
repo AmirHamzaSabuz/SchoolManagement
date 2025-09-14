ac6fee99be2a495b07c77f144a6bc12b4c05813f
Amir Hamza <hamza_amir@yahoo.com>
2025-09-14 23:34:44 +0600

    Create eum BloodGroup and UserRose

---
+++ SchoolManagement.Core/Enums/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Enums
+{
+    public enum UserRole
+    {
+        Admin = 1,
+        Teacher = 2,
+        Student = 3,
+        Parent = 4,
+        Staff = 5
+    }
+}
