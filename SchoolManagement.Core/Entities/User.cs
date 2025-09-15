acacdd2e1d7303800fd263c70e8a3b1ef6b8b8c2
Amir Hamza <hamza_amir@yahoo.com>
2025-09-15 22:53:45 +0600

    Create User Class

---
+++ SchoolManagement.Core/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using SchoolManagement.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Entities
+{
+    public class User: BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+        public string? ProfilePicture { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<StudentEnrollment> StudentEnrollments { get; set; } = new List<StudentEnrollment>();
+        public virtual ICollection<TeacherAssignment> TeacherAssignments { get; set; } = new List<TeacherAssignment>();
+    }
+}
