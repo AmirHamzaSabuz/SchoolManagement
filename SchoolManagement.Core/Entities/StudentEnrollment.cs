1374ae263e61650e5ba8b0896ad49534e5202be8
Amir Hamza <hamza_amir@yahoo.com>
2025-09-25 05:07:25 +0600

    class StudentEnrollment.cs

--- SchoolManagement.Core/Entities/StudentEnrollment.cs
+++ SchoolManagement.Core/Entities/StudentEnrollment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Entities
 {
-    public class StudentEnrollment
+    public class StudentEnrollment : BaseEntity
     {
+        public int StudentId { get; set; }
+        public int ClassId { get; set; }
+        public string RollNumber { get; set; } = string.Empty;
+        public DateTime EnrollmentDate { get; set; }
+        public string AcademicYear { get; set; } = string.Empty;
+        public BloodGroup BloodGroup { get; set; }
+        public string EmergencyContactName { get; set; } = string.Empty;
+        public string EmergencyContactPhone { get; set; } = string.Empty;
+
+        // Navigation properties
+        public virtual User Student { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+        public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
+        public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
     }
 }
