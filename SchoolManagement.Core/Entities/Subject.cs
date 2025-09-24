fd047f0232d11bd2f64ffaab6a931946d00a18d8
Amir Hamza <hamza_amiir@yahoo.com>
2025-09-25 00:25:44 +0600

    Create class Subject.cs

--- SchoolManagement.Core/Entities/Subject.cs
+++ SchoolManagement.Core/Entities/Subject.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Entities
 {
-    public class Subject
+    public class Subject : BaseEntity
     {
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Credits { get; set; }
+        public int ClassId { get; set; }
+
+        // Navigation properties
+        public virtual Class Class { get; set; } = null!;
+        public virtual ICollection<TeacherAssignment> TeacherAssignments { get; set; } = new List<TeacherAssignment>();
+        public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
+        public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
     }
 }
