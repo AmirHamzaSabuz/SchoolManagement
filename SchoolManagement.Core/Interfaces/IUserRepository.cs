562760235b18bcc733eff6168fe10896d4fe9a53
Amir Hamza <hamza_amiir@yahoo.com>
2025-09-25 23:26:26 +0600

    interface IUserRepository

---
+++ SchoolManagement.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using SchoolManagement.Core.Entities;
+using SchoolManagement.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<IReadOnlyList<User>> GetUsersByRoleAsync(UserRole role);
+        Task<bool> IsEmailExistsAsync(string email);
+        Task<IReadOnlyList<User>> GetTeachersAsync();
+        Task<IReadOnlyList<User>> GetStudentsAsync();
+    }
+}
