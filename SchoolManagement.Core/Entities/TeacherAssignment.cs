0287ab994125903a4d906959d4453e62ab8a334b
Amir Hamza <hamza_amir@yahoo.com>
2025-09-25 19:03:46 +0600

    Class TeacherAssignment.cs

--- SchoolManagement.Core/Entities/TeacherAssignment.cs
+++ SchoolManagement.Core/Entities/TeacherAssignment.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Entities
 {
-    public class TeacherAssignment
+    public class TeacherAssignment : BaseEntity
     {
+        public int TeacherId { get; set; }
+        public int ClassId { get; set; }
+        public int SubjectId { get; set; }
+        public string AcademicYear { get; set; } = string.Empty;
 
+        // Navigation properties
+        public virtual User Teacher { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+        public virtual Subject Subject { get; set; } = null!;
     }
 }
