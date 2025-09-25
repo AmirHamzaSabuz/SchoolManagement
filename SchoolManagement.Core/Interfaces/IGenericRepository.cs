6bb2dc646fb4020cc4418f656f0a06d1b9ef2a92
Amir Hamza <hamza_amiir@yahoo.com>
2025-09-25 23:20:54 +0600

    interface IGenericRepository

---
+++ SchoolManagement.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate,
+                                       Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                                       string includeString);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate,
+                                       Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+                                       List<Expression<Func<T, object>>> includes);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    }
+}
