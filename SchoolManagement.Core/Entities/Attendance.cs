5022a9c839aca3b13ab2242819c2485aa83d0a38
Amir Hamza <hamza_amiir@yahoo.com>
2025-09-25 19:18:07 +0600

    class Attendance.cs

--- SchoolManagement.Core/Entities/Attendance.cs
+++ SchoolManagement.Core/Entities/Attendance.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Entities
 {
-    public class Attendance
+    public class Attendance : BaseEntity
     {
+        public int StudentEnrollmentId { get; set; }
+        public int SubjectId { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPresent { get; set; }
+        public string? Remarks { get; set; }
+
+        // Navigation properties
+        public virtual StudentEnrollment StudentEnrollment { get; set; } = null!;
+        public virtual Subject Subject { get; set; } = null!;
     }
 }
