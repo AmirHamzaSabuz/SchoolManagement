680ed650a88ba188a5a25bce1c4964cac0e1db43
Amir Hamza <hamza_amir@yahoo.com>
2025-09-25 23:59:16 +0600

    interface IUnitOfWork

---
+++ SchoolManagement.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using SchoolManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IClassRepository Classes { get; }
+        IGenericRepository<Subject> Subjects { get; }
+        IStudentEnrollmentRepository StudentEnrollments { get; }
+        IGenericRepository<TeacherAssignment> TeacherAssignments { get; }
+        IGenericRepository<Grade> Grades { get; }
+        IGenericRepository<Attendance> Attendances { get; }
+
+        Task<int> CompleteAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
