78c751705ae2d22d25abd715d099e5876290411e
Amir Hamza <hamza_amir@yahoo.com>
2025-09-25 23:36:00 +0600

    interface IClassRepository

---
+++ SchoolManagement.Core/Interfaces/IClassRepository.cs
@@ -0,0 +1,16 @@
+﻿using SchoolManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Interfaces
+{
+    public interface IClassRepository : IGenericRepository<Class>
+    {
+        Task<IReadOnlyList<Class>> GetClassesWithTeacherAsync();
+        Task<Class?> GetClassWithSubjectsAsync(int classId);
+        Task<IReadOnlyList<Class>> GetAvailableClassesAsync();
+    }
+}
