6e7ab8b5a3cef83dce70c71c0b0f2e2916729086
Amir Hamza <hamza_amir@yahoo.com>
2025-09-25 23:52:27 +0600

    interface IStudentEnrollmentRepository

---
+++ SchoolManagement.Core/Interfaces/IStudentEnrollmentRepository .cs
@@ -0,0 +1,17 @@
+﻿using SchoolManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Core.Interfaces
+{
+    public interface IStudentEnrollmentRepository : IGenericRepository<StudentEnrollment>
+    {
+        Task<StudentEnrollment?> GetByStudentIdAndClassIdAsync(int studentId, int classId);
+        Task<IReadOnlyList<StudentEnrollment>> GetEnrollmentsByClassAsync(int classId);
+        Task<IReadOnlyList<StudentEnrollment>> GetEnrollmentsByStudentAsync(int studentId);
+        Task<string> GenerateRollNumberAsync(int classId);
+    }
+}
