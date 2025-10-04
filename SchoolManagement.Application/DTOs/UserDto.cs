6692dd6240f56e62998a3c5f8ed25ebc35883a92
Amir Hamza <hamza_amir@yahoo.com>
2025-10-04 23:58:06 +0600

    UserDto

---
+++ SchoolManagement.Application/DTOs/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using SchoolManagement.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+        public string? ProfilePicture { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
